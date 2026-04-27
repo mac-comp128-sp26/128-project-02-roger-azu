b5e3fcb70c50f89e29ca6442e6308074d658b985
rfranciscooo <rcarranz@macalester.edu>
2026-04-27 13:27:47 -0500

    implementing graph structure

--- src/AdjacencyListGraph.java
+++ src/AdjacencyListGraph.java
@@ -1,17 +1,206 @@
+package graphImplementation.finalProject;
+
+
 import java.util.LinkedList;
+import java.util.NoSuchElementException;
+import java.util.Scanner;
+import java.util.Stack;
+
 
 public class AdjacencyListGraph {
     
     private final int V;
     private int E;
-    private Bag<Integer> [] adj;
+    private Bag<Edge>[] adj;
 
-    // Initialize adjacency list
+    /**
+     * Initializes an empty graph with {@code V} vertices and 0 edges.
+     * param V the number of vertices
+     *
+     * @param V number of vertices
+     * @throws IllegalArgumentException if {@code V < 0}
+     */
+    public AdjacencyListGraph(int V) {
+        if (V < 0) throw new IllegalArgumentException("Number of vertices must be nonnegative");
+        this.V = V;
+        this.E = 0;
+        adj = (Bag<Edge>[]) new Bag[V];
+        for (int v = 0; v < V; v++) {
+            adj[v] = new Bag<Edge>();
+        }
+    }
+
+    class Edge{
+        int to;
+        double weight;
+
+        Edge(int to, double weight){
+            this.to = to;
+            this.weight = weight;
+        }
+    }
+
+    /**
+     * Initializes a graph from the specified input stream.
+     * The format is the number of vertices <em>V</em>,
+     * followed by the number of edges <em>E</em>,
+     * followed by <em>E</em> pairs of vertices, with each entry separated by whitespace.
+     *
+     * @param in the input stream
+     * @throws IllegalArgumentException if {@code in} is {@code null}
+     * @throws IllegalArgumentException if the endpoints of any edge are not in prescribed range
+     * @throws IllegalArgumentException if the number of vertices or edges is negative
+     * @throws IllegalArgumentException if the input stream is in the wrong format
+     */
+    public AdjacencyListGraph(Scanner in) {
+        if (in == null) throw new IllegalArgumentException("argument is null");
+        try {
+            this.V = in.nextInt();
+            if (V < 0) throw new IllegalArgumentException("number of vertices in a Graph must be nonnegative");
+            adj = (Bag<Edge>[]) new Bag[V];
+            for (int v = 0; v < V; v++) {
+                adj[v] = new Bag<Edge>();
+            }
+            int E = in.nextInt();
+            if (E < 0) throw new IllegalArgumentException("number of edges in a Graph must be nonnegative");
+            for (int i = 0; i < E; i++) {
+                int v = in.nextInt();
+                int w = in.nextInt();
+                validateVertex(v);
+                validateVertex(w);
+                addEdge(v, w);
+            }
+        } catch (NoSuchElementException e) {
+            throw new IllegalArgumentException("invalid input format in Graph constructor", e);
+        }
+    }
 
 
-    // Add routes to the adjacency list
+    /**
+     * Initializes a new graph that is a deep copy of {@code G}.
+     *
+     * @param G the graph to copy
+     * @throws IllegalArgumentException if {@code G} is {@code null}
+     */
+    public AdjacencyListGraph(Graph G) {
+        this.V = G.V();
+        this.E = G.E();
+        if (V < 0) throw new IllegalArgumentException("Number of vertices must be nonnegative");
+
+        // update adjacency lists
+        adj = (Bag<Edge>[]) new Bag[V];
+        for (int v = 0; v < V; v++) {
+            adj[v] = new Bag<Edge>();
+        }
+
+        for (int v = 0; v < G.V(); v++) {
+            // reverse so that adjacency list is in same order as original
+            Stack<Integer> reverse = new Stack<Integer>();
+            for (int w : G.adj(v)) {
+                reverse.push(w);
+            }
+            for (int w : reverse) {
+                adj[v].add(w);
+            }
+        }
+    }
+
+    /**
+     * Returns the number of vertices in this graph.
+     *
+     * @return the number of vertices in this graph
+     */
+    public int V() {
+        return V;
+    }
+
+    /**
+     * Returns the number of edges in this graph.
+     *
+     * @return the number of edges in this graph
+     */
+    public int E() {
+        int count = 0;
+        for (int i=0; i < adj.length; i++) {
+            count += adj[i].size();
+        }
+        return count;
+    }
+
+    // throw an IllegalArgumentException unless {@code 0 <= v < V}
+    private void validateVertex(int v) {
+        if (v < 0 || v >= V)
+            throw new IllegalArgumentException("vertex " + v + " is not between 0 and " + (V - 1));
+    }
+
+    /**
+     * Adds the undirected edge v-w to this graph.
+     *
+     * @param v one vertex in the edge
+     * @param w the other vertex in the edge
+     * @throws IllegalArgumentException unless both {@code 0 <= v < V} and {@code 0 <= w < V}
+     */
+    public void addEdge(int v, int w, double weight) {
+        validateVertex(v);
+        validateVertex(w);
+        adj[v].add(new Edge(w, weight));
+        E = E + 1;
+        }
+
+    /**
+     * Returns the vertices adjacent to vertex {@code v}.
+     *
+     * @param v the vertex
+     * @return the vertices adjacent to vertex {@code v}, as an iterable
+     * @throws IllegalArgumentException unless {@code 0 <= v < V}
+     */
+    public Iterable<Edge> adj(int v) {
+        validateVertex(v);
+        return adj[v];
+    }
+
+    /**
+     * Returns the degree of vertex {@code v}.
+     *
+     * @param v the vertex
+     * @return the degree of vertex {@code v}
+     * @throws IllegalArgumentException unless {@code 0 <= v < V}
+     */
+    public int degree(int v) {
+        validateVertex(v);
+        return adj[v].size();
+    }
 
 
-    // Method implementing Dijkstra’s algorithm
+    /**
+     * Returns a string representation of this graph.
+     *
+     * @return the number of vertices <em>V</em>, followed by the number of edges <em>E</em>,
+     * followed by the <em>V</em> adjacency lists
+     */
+    public String toString() {
+        StringBuilder s = new StringBuilder();
+        s.append(V + " vertices, " + E + " edges " + NEWLINE);
+        for (int v = 0; v < V; v++) {
+            s.append(v + ": ");
+            for (int w : adj[v]) {
+                s.append(w + " ");
+            }
+            s.append(NEWLINE);
+        }
+        return s.toString();
+    }
+
+    /**
+     * Tests the {@code Graph} data type.
+     *
+     * @param args the command-line arguments
+     */
+    public static void main(String[] args) {
+        Graph G = new AdjacencyListGraph(3);
+        G.addEdge(1,2);
+        G.addEdge(0,2);
+        System.out.println(G);
+    }
 
 }

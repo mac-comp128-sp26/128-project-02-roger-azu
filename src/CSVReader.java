6f6affc362eab1f378a17b15be2888072d6c830e
Azucena-Ventimilla <ventimillazucena.64@gmail.com>
2026-04-28 18:01:50 -0500

    Setting up CSVReader class
    Utils class that will be used to extract data

--- src/CSVReader.java
+++ src/CSVReader.java
@@ -1,3 +1,27 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class CSVReader {
-    
+
+    public static void main(String[] args) {
+        String path = "/Users/azu_v22/Desktop/greenhouse-gas-emissions.csv";
+        String line = "";
+
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(path));
+            while ((line = br.readLine()) != null) {
+                String [] values = line.split(",");
+                System.out.println(values[9]);
+            }
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
 }

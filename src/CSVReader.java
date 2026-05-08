f293794ed0f632f279706cdb09905b8056f34f5f
Azucena-Ventimilla <ventimillazucena.64@gmail.com>
2026-05-07 19:31:24 -0500

    Edit

--- src/CSVReader.java
+++ src/CSVReader.java
@@ -2,26 +2,59 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.HashMap;
 
 public class CSVReader {
 
-    public static void main(String[] args) {
-        String path = "/Users/azu_v22/Desktop/greenhouse-gas-emissions.csv"; // need to update data in res folder
+
+    public CSVReader() {
+
+    }
+
+    public void loadStopFile(String path) {
         String line = "";
+        int stopId = 0;
+        HashMap<String, Integer> stopIdMap = new HashMap<String, Integer>();
+        
 
         try {
             BufferedReader br = new BufferedReader(new FileReader(path));
+            br.readLine(); // skip header
             while ((line = br.readLine()) != null) {
-                String [] values = line.split(",");
-                System.out.println(values[9]);
+                String[] values = line.split(",");
+                stopIdMap.put(values[0], stopId); // stopId with stopIndex map
+                stopId ++;
             }
-
+            br.close();
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
+
+    public static void main(String[] args) {
+        // String path = "/Users/azu_v22/Desktop/greenhouse-gas-emissions.csv"; // need to update data in res folder
+        // String line = "";
+
+        // try {
+        //     BufferedReader br = new BufferedReader(new FileReader(path));
+        //     while ((line = br.readLine()) != null) {
+        //         String[] values = line.split(",");
+        //         System.out.println(values[9]);
+        //     }
+
+        // } catch (FileNotFoundException e) {
+        //     e.printStackTrace();
+        // } catch (IOException e) {
+        //     e.printStackTrace();
+        // }
+        String stopsPath = "res/gtfs/stops.txt";
+        CSVReader csvReader = new CSVReader();
+        csvReader.loadStopFile(stopsPath);
+
+    }
+
 
 
 }
